42b1675af7705c01793ed8970cbec07e2290e36e
email <imrealleonardo@gmail.com>
2022-01-22 03:05:35 +0800

    test: dynamic meta

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
+  <metainfo>
+    <template #base="{ content }">{{ content }} </template>
+    <template #title="{ content, metainfo }">
+      {{ content }} - {{ metainfo.description }}
+    </template>
+  </metainfo>
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useMeta } from "vue-meta";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    useMeta({
+      charset: "utf8",
+      og: {
+        locale: "en_US",
+        type: "website",
+        site_name: "VueSeoFriendlySpa",
+      },
+      htmlAttrs: {
+        amp: true,
+        lang: ["en"],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: "Twitter Title",
+        site: "star-history.com",
+        creator: "bytebase",
+        description: "123",
+        image: "https://star-history.com/star-history.webp",
+      },
+    });
+  },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createMetaManager } from "vue-meta";
 import router from "./router";
 import App from "./App.vue";
 import { piniaInstance } from "./store";
@@ -6,4 +7,8 @@ import "./css/index.css";
 
 const app = createApp(App);
 
-app.use(router).use(piniaInstance).mount("#app");
+app.use(router).use(createMetaManager(false)).use(piniaInstance);
+
+router.isReady().then(() => {
+  app.mount("#app");
+});
