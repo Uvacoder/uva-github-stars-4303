4ccff040c4d916c376d94c8b8de163c1117597ec
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-04 15:42:44 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,25 +9,25 @@
         class="text-sm leading-8 flex flex-row flex-wrap justify-start items-center"
       >
         <h1 class="h-full text-gray-600">
-          The missing GitHub star history graph
+          Whats the star history for a github repo?  Whats it look like charted?
         </h1>
         <a
           class="h-full flex flex-row justify-center items-center ml-3 text-lg hover:opacity-80"
-          href="https://twitter.com/StarHistoryHQ"
+          href="https://twitter.com/AlecCam43544378"
           target="_blank"
         >
           <i class="fab fa-twitter"></i>
         </a>
         <a
           class="h-full flex flex-row justify-center items-center mx-3 text-lg hover:opacity-80"
-          href="mailto:star@bytebase.com"
+          href="mailto:freeunioncoder@gmail.com"
           target="_blank"
         >
           <i class="fas fa-envelope"></i>
         </a>
         <a
           class="h-full flex flex-row justify-center items-center mr-3 text-lg hover:opacity-80"
-          href="https://github.com/bytebase/star-history"
+          href="https://github.com/uvacoder/star-charts"
           target="_blank"
         >
           <i class="fab fa-github"></i>
@@ -37,19 +37,19 @@
         class="text-xs leading-8 flex flex-row flex-nowrap justify-end items-center"
       >
         <span class="text-gray-600">
-          Sponsored by
+          Brought to you from
           <a
             class="text-blue-500 font-bold hover:opacity-80"
-            href="https://bytebase.com"
+            href="https://abclinks.vercel.app"
             target="_blank"
-            >Bytebase</a
-          >, originally built by
+            >abclinks</a
+          >, on GitHub 
           <a
             class="bg-blue-400 text-white p-1 pl-2 pr-2 rounded-l-2xl rounded-r-2xl hover:opacity-80"
-            href="https://twitter.com/tim_qian"
+            href="https://github.com/uvacoder"
             target="_blank"
           >
-            @tim_qian
+            @uvacoder
           </a>
         </span>
       </div>
